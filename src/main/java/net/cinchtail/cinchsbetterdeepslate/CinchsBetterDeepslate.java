7f6a95166ecdf5eebc98c6e86550f313a8060dd8
cinchtail <cinchtail@gmail.com>
2024-04-23 23:47:13 -0400

    Initial commit

---
+++ src/main/java/net/cinchtail/cinchsbetterdeepslate/CinchsBetterDeepslate.java
@@ -0,0 +1,46 @@
+package net.cinchtail.cinchsbetterdeepslate;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(CinchsBetterDeepslate.MOD_ID)
+public class CinchsBetterDeepslate {
+    public static final String MOD_ID = "cinchsbetterdeepslate";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public CinchsBetterDeepslate() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+    }
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+    }
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
